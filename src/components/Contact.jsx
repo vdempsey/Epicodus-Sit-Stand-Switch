36b48272a856527dd13794002ee94309e895824f
Valia Dempsey <valentinaml@gmail.com>
2018-09-12 11:43:55 -0700

    Fix link for Contact view, remove unused HeadeeUser component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -12,7 +12,7 @@ function Contact(){
       <BannerTwo bannerText='Activate your 8 to 5' />
       <Menu />
       <div>
-        <h2>Future Headline goes here</h2>
+        <h2>Future Contact Headline goes here</h2>
         <p>
           Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
         </p>
